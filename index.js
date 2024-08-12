3e332435488951f13fac9ca90ac5ef0d261babe3
hoand <hoaace2003@gmail.com>
2024-08-12 22:24:14 +0700

    hello world

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("xin chao VietNam");
+  res.send("xin chao VietNam, toi la hoa day");
 });
 
 app.listen(port, () => {
